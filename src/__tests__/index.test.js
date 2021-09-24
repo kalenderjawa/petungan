97c7a5d8a8cc1899bd2df8793385d7a521f8b5c8
Equan Pr <equan.p@gmail.com>
2021-09-24 10:37:53 +0700

    test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,11 +1,21 @@
-import { tabelKonstantaKonversiTahunJawa, tabelKonstantaKonversiTahunMasehi } from '../index.js'
+import {
+  tabelKonstantaKonversiTahunJawa,
+  tabelKonstantaKonversiTahunMasehi,
+  cariTahunReferensi,
+} from "../index.js";
 
-describe('Tabel Konstanta Konversi Tahun', () => {
-	it("Cek jumlah data konstanta Jawa", () => {
-		expect(tabelKonstantaKonversiTahunJawa().length).toBe(78)
-	})
+describe("Tabel Konstanta Konversi Tahun", () => {
+  it("Cek jumlah data konstanta Jawa", () => {
+    expect(tabelKonstantaKonversiTahunJawa().length).toBe(78);
+  });
 
-	it("Cek jumlah data konstanta Masehi", () => {
-		expect(tabelKonstantaKonversiTahunMasehi().length).toBe(78)
-	})
-})
+  it("Cek jumlah data konstanta Masehi", () => {
+    expect(tabelKonstantaKonversiTahunMasehi().length).toBe(78);
+  });
+});
+
+describe("Test Cari Tahun Referensi", () => {
+  it("konstanta konversi tahun masehi", () => {
+    expect(cariTahunReferensi(tabelKonstantaKonversiTahunJawa(), 2000)?.konstan).toBe(68);
+  });
+});
