421bbe3905a46633225f399ec6bbc3e5811cc4b1
Equan Pr <equan.p@gmail.com>
2021-09-25 15:09:31 +0700

    test konversi tahun Jawa ke Hijriyah

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -4,6 +4,11 @@ import {
   cariTahunReferensi,
 } from "../index.js";
 
+import {
+  konversiTahunJawaKeTahunHijriyah,
+  konversiTahunHijriyahKeTahunJawa,
+} from "../jh.js";
+
 describe("Tabel Konstanta Konversi Tahun", () => {
   it("Cek jumlah data konstanta Jawa", () => {
     expect(tabelKonstantaKonversiTahunJawa().length).toBe(78);
@@ -16,25 +21,48 @@ describe("Tabel Konstanta Konversi Tahun", () => {
 
 describe("Test Cari Konstanta Konversi Tahun Referensi Jawa", () => {
   it("Konstanta Konversi Tahun Jawa 1555 adalah 78", () => {
-    expect(cariTahunReferensi(tabelKonstantaKonversiTahunJawa(), 1555)?.konstan).toBe(78);
+    expect(
+      cariTahunReferensi(tabelKonstantaKonversiTahunJawa(), 1555)?.konstan
+    ).toBe(78);
   });
   it("Konstanta Konversi Tahun Jawa 2000 adalah 65", () => {
-    expect(cariTahunReferensi(tabelKonstantaKonversiTahunJawa(), 2000)?.konstan).toBe(65);
+    expect(
+      cariTahunReferensi(tabelKonstantaKonversiTahunJawa(), 2000)?.konstan
+    ).toBe(65);
   });
   it("Konstanta Konversi Tahun Jawa 3288 adalah 28", () => {
-    expect(cariTahunReferensi(tabelKonstantaKonversiTahunJawa(), 3288)?.konstan).toBe(28);
+    expect(
+      cariTahunReferensi(tabelKonstantaKonversiTahunJawa(), 3288)?.konstan
+    ).toBe(28);
   });
 });
 
 describe("Test Cari Konstanta Konversi Tahun Referensi Masehi", () => {
   it("Konstanta Konversi Tahun Jawa 1633 adalah 78", () => {
-    expect(cariTahunReferensi(tabelKonstantaKonversiTahunMasehi(), 1633)?.konstan).toBe(78);
+    expect(
+      cariTahunReferensi(tabelKonstantaKonversiTahunMasehi(), 1633)?.konstan
+    ).toBe(78);
   });
   it("Konstanta Konversi Tahun Jawa 2022 adalah 67", () => {
-    expect(cariTahunReferensi(tabelKonstantaKonversiTahunMasehi(), 2022)?.konstan).toBe(67);
+    expect(
+      cariTahunReferensi(tabelKonstantaKonversiTahunMasehi(), 2022)?.konstan
+    ).toBe(67);
   });
   it("Konstanta Konversi Tahun Jawa 2890 adalah 42", () => {
-    expect(cariTahunReferensi(tabelKonstantaKonversiTahunMasehi(), 2890)?.konstan).toBe(42);
+    expect(
+      cariTahunReferensi(tabelKonstantaKonversiTahunMasehi(), 2890)?.konstan
+    ).toBe(42);
   });
 });
 
+describe("Test Konversi Jawa Ke Hijriyah", () => {
+  it("Konversi Tahun Jawa 1555 adalah 1043", () => {
+    expect(konversiTahunJawaKeTahunHijriyah(1555)).toBe(1043);
+  });
+  it("Konversi Tahun Jawa 1955 adalah 1443", () => {
+    expect(konversiTahunJawaKeTahunHijriyah(1955)).toBe(1443);
+  });
+  it("Konversi Tahun Jawa 2500 adalah 1500", () => {
+    expect(konversiTahunJawaKeTahunHijriyah(1500)).toBe(1500);
+  });
+})
