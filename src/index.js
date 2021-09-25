79884cdea6f8a37e8123ac796b2119bceab0dbfd
Equan Pr <equan.p@gmail.com>
2021-09-25 15:09:40 +0700

    re export

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ export {
   tabelKonstantaKonversiTahunMasehi,
   cariTahunReferensi,
 } from "./pelok.js";
+
+export {
+  konversiTahunJawaKeTahunHijriyah,
+  konversiTahunHijriyahKeTahunJawa,
+} from "./jh.js";
