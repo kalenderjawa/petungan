564e146c692bf300c79d915764c1746bc32e9d5f
Equan Pr <equan.p@gmail.com>
2021-09-25 15:08:55 +0700

    prototipe konversi Jawa Hijriyah

--- src/jh.js
+++ src/jh.js
@@ -1,21 +1,32 @@
 /**
+ * Referensi
+ * https://github.com/kalenderjawa/cathetan/blob/main/KONVERSI.md#Konversi-Tahun-Jawa-Ke-Tahun-Hijriyah
+ */
+
+const KONSTANTA_KONVERSI_HIJRIYAH = 512;
+const AWAL_TAHUN_JAWA = 1555;
+const AWAL_TAHUN_HIJRIYAH = 1043;
+
+/**
  * Konversi tahun Jawa ke Tahun Hijriyah
- * @param {Number} tahun
+ * @param {Number} tahunJawa
  * @returns
  */
-function konversiTahunJawaKeTahunHijriyah(tahun) {
-  //TODO
-  return tahun;
+function konversiTahunJawaKeTahunHijriyah(tahunJawa) {
+  return tahunJawa < AWAL_TAHUN_JAWA
+    ? tahunJawa
+    : tahunJawa - KONSTANTA_KONVERSI_HIJRIYAH;
 }
 
 /**
- *
- * @param {Number} tahun
+ * Konversi tahun Hijriyah ke Tahun Jawa
+ * @param {Number} tahunHijriyah
  * @returns
  */
-function konversiTahunHijriyahKeTahunJawa(tahun) {
-  //TODO
-  return tahun;
+function konversiTahunHijriyahKeTahunJawa(tahunHijriyah) {
+  return tahunHijriyah < AWAL_TAHUN_HIJRIYAH
+    ? tahunHijriyah
+    : tahunHijriyah + KONSTANTA_KONVERSI_HIJRIYAH;
 }
 
 export { konversiTahunJawaKeTahunHijriyah, konversiTahunHijriyahKeTahunJawa };
