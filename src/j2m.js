6d78b5b37361f909cf459bb66577521a100fd45e
Equan Pr <equan.p@gmail.com>
2021-09-25 07:57:19 +0700

    jawa - masehi

---
+++ src/j2m.js
@@ -0,0 +1,21 @@
+/**
+ * Konversi tahun masehi ke tahun jawa
+ * @param {Number} tahun
+ * @returns
+ */
+function konversiTahunMasehiKeTahunJawa(tahun) {
+  //TODO
+  return tahun;
+}
+
+/**
+ * Konversi tahun jawa ke tahun masehi
+ * @param {Number} tahun
+ * @returns
+ */
+function konversiTahunJawaKeTahunMasehi(tahun) {
+  //TODO
+  return tahun;
+}
+
+export { konversiTahunMasehiKeTahunJawa, konversiTahunJawaKeTahunMasehi };
