1b8b9d7b4588125c1e9ff96b93f05a7c9ef3bf31
Equan Pr <equan.p@gmail.com>
2021-09-26 06:27:54 +0700

    test konversi tahun masehi ke jawa

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -88,3 +88,18 @@ describe("Test konversi Jawa ke Masehi", () => {
     expect(konversiTahunJawaKeTahunMasehi(1555)).toBe(1633);
   });
 });
+
+describe("Test konversi Masehi ke Jawa", () => {
+  it("Konversi Tahun Masehi 1633 ke Jawa adalah 1555", () => {
+    expect(konversiTahunMasehiKeTahunJawa(1633)).toBe(1555);
+  });
+
+  it("Konversi Tahun Masehi 1983 ke Jawa adalah 1555", () => {
+    expect(konversiTahunMasehiKeTahunJawa(1983)).toBe(1915);
+  });
+
+
+  it("Konversi Tahun Masehi 2022 ke Jawa adalah 1955", () => {
+    expect(konversiTahunMasehiKeTahunJawa(2022)).toBe(1955);
+  });
+})
