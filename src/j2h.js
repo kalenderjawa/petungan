5ec78990313dc43e19e84cd54038bcbeaac3a292
Equan Pr <equan.p@gmail.com>
2021-09-25 07:57:00 +0700

    jawa - hijriyah

---
+++ src/j2h.js
@@ -0,0 +1,21 @@
+/**
+ * Konversi tahun Jawa ke Tahun Hijriyah
+ * @param {Number} tahun
+ * @returns
+ */
+function konversiTahunJawaKeTahunHijriyah(tahun) {
+  //TODO
+  return tahun;
+}
+
+/**
+ *
+ * @param {Number} tahun
+ * @returns
+ */
+function konversiTahunHijriyahKeTahunJawa(tahun) {
+  //TODO
+  return tahun;
+}
+
+export { konversiTahunJawaKeTahunHijriyah, konversiTahunHijriyahKeTahunJawa };
